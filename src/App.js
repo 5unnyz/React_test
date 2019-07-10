f9baf4df28b3a683264d0dab50fd2d6982b89ff7
5unnyz <sumitsunny143@gmail.com>
2019-07-10 11:03:47 +0530

    list_update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Data from './New_assig/New_assig';
 import Info from './New_assig/Second_assig';
+import Friend from './New_assig/New_assig';
 
 class App extends Component {
 state = {
   Friends: [
-    {name: 'a' , age: 25},
-    {name: 'b' , age: 28},
-    {name: 'c' , age: 29}
+    {id:'1' ,name: 'a' , age: 25},
+    {id:'2' ,name: 'b' , age: 28},
+    {id:'3' ,name: 'c' , age: 29}
   ],
   showperson : true
 
@@ -40,32 +41,72 @@ secondstate ={
     ]
   })
 }
-changename = (event) =>
-{
-  this.setState({
-    Friends:[
-    {name: 'e' , age: 25},
-    {name: event.target.value , age: 28},
-    {name: 'g' , age: 29}
-    ]
-  })
-}
+// changename = (event) =>
+// {
+//   this.setState({
+//     Friends:[
+//     {name: 'e' , age: 25},
+//     {name: event.target.value , age: 28},
+//     {name: 'g' , age: 29}
+//     ]
+//   })
+
 toggledetail = () =>
 {
   const doesshow = this.state.showperson;
   console.log(doesshow);
   this.setState({showperson:!doesshow})
 }
+deletehandler = (friendindex) =>
+{
+  const friend = this.state.Friends;
+  friend.splice(friendindex,1)
+  this.setState({Friends:friend})
+}
 
-  render () {
+  render () 
+  {
+    let data_all = null;
+    if(this.state.showperson)
+    { data_all = (
+      <div>
+        {this.state.Friends.map((friend, index) =>{
+          return <Friend
+          click ={()=>this.deletehandler(index)}
+          name = {friend.name}
+          age = {friend.age}
+          key ={friend.id}>
+          </Friend>
+        })}
+        </div>
+    );
+      {/* <Data
+        name= {this.state.Friends[0].name}
+        age ={this.state.Friends[0].age} />
+        <Data
+        name= {this.state.Friends[1].name}
+        age ={this.state.Friends[1].age}
+        changed={this.changename}/>
+        <Data
+        name= {this.state.Friends[2].name}
+        age ={this.state.Friends[2].age}
+        click = {this.Changestate} /> */}
+        //</div>
+    
+    }
+  
+  
+  
+    
   return (
     <div className="App">
       <p>
         <button onClick = {this.toggledetail}>For Change</button>
+        {data_all}
 
       </p>
-      <p>hi</p>
-      {this.state.showperson ? 
+      
+      {/* {this.state.showperson ? 
       <div>
       <p>
         <Data
@@ -86,11 +127,13 @@ toggledetail = () =>
         time = {this.secondstate.enemy[0].time} />
         </p>
         </div> :null
-      }
+      } */}
         
     </div>
-  );
+  )
+    
+    }  
   }
-}
-
+    
+  
 export default App;
